d7097b23a11d7bfeac4d979916c7a37985e8a25f
22340ll <22340ll@hvhs.school.nz>
2026-02-27 15:06:10 +1300

    12314123fr

--- games/BlandbournBout/MJS/BbB.js
+++ games/BlandbournBout/MJS/BbB.js
@@ -1,5 +1,23 @@
-function setup() {
-    // Initialize Firebase  
-    fb_initialize();
-    
+import { fb_initialise } from "../../../fb_io.mjs";
+
+fb_initialise
+/*******************************************************/
+function preload() {
+    imgPlaceholder = loadImage('../other/image.jpg');
+    imgSpartan = loadImage('../other/Kratos_PS4.png');
+    imgWizard = loadImage('../other/');
+    imgPalidin = loadImage('../other/');
+    imgBardarian = loadImage('../other/');
+    imgCleric = loadImage('../other/');
+
+
+
+}
+
+function setup() { //Setup. Yes this needs to happen.
+    createCanvas(windowWidth, windowHeight);
+    funcSetupScore();
+    setupHealthDisplayU();
+    setupHealthDisplayOpp();
+    funcImg();
 }
