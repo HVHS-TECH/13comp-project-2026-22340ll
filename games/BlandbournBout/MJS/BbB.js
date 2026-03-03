80bc56216d94910a470c6a10f531de1fdae959fe
22340ll <22340ll@hvhs.school.nz>
2026-03-04 11:58:15 +1300

    34t5i

--- games/BlandbournBout/MJS/BbB.js
+++ games/BlandbournBout/MJS/BbB.js
@@ -1,23 +1,39 @@
-import { fb_initialise } from "../../../fb_io.mjs";
+import {  fb_initialize, fb_authChanged }
+from "../../../fb_io.mjs"; //Import
 
-fb_initialise
-/*******************************************************/
-function preload() {
-    imgPlaceholder = loadImage('../other/image.jpg');
-    imgSpartan = loadImage('../other/Kratos_PS4.png');
-    imgWizard = loadImage('../other/');
-    imgPalidin = loadImage('../other/');
-    imgBardarian = loadImage('../other/');
-    imgCleric = loadImage('../other/');
-
-
-
-}
-
-function setup() { //Setup. Yes this needs to happen.
+function setup() {
     createCanvas(windowWidth, windowHeight);
     funcSetupScore();
     setupHealthDisplayU();
     setupHealthDisplayOpp();
     funcImg();
+
+    // Initialize Firebase  
+    fb_initialize();
+    // Listen for auth state changes
+    fb_authChanged(user => {
+        if (user) {
+            console.log('User signed in:', user);
+            // theres a user
+        } else {
+            console.log('No user signed in');
+            // theres no user
+        }
+    });
+  
 }
+
+fb_initialize();
+/*******************************************************/
+function preload() { //Preload everyting for further purposes.
+    imgPlaceholder = loadImage('../other/image.jpg'); //placeholder
+    imgSpartan = loadImage('../other/Kratos_PS4.png'); // all rights go to sony and playstation
+    imgWizard = loadImage('../other/');
+    imgPalidin = loadImage('../other/');
+    imgBardarian = loadImage('../other/');
+    imgCleric = loadImage('../other/');
+    imgBG1 = loadImage('../other/');
+    imgBG2 = loadImage('../other/');
+
+}
+
